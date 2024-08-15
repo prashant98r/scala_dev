49a09b2c3d5cb7c011ce40ee9e9043cb8510d7de
kprashant <prashant98r@gmail.com>
2024-08-15 14:58:41 +0530

    Check for Even and Positive number

---
+++ src/main/scala/evenAndPositive.scala
@@ -0,0 +1,11 @@
+object evenAndPositive {
+  def main(args:Array[String]):Unit={
+    var a:Int=13
+    if(a%2==0&&a>0){
+      println("Even and Positive")
+    }
+    else{
+      println("Not even or not positive")
+    }
+  }
+}
