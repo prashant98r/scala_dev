c11755cb2593c8b44986930c3df4594ea81cfb71
kprashant <prashant98r@gmail.com>
2024-08-15 15:23:57 +0530

    Range Check with OR

--- src/main/scala/rangeCheckWithOr.scala
+++ src/main/scala/rangeCheckWithOr.scala
@@ -1,7 +1,7 @@
 object rangeCheckWithOr {
   def main(args:Array[String]):Unit={
-    var a: Int = -5
-    var c: Boolean = ( a< -10 || a>10 )
+    val a: Int = -5
+    val c: Boolean = a< -10 || a>10
       println(c)
   }
 }
