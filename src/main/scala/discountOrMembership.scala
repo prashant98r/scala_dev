ac1ca432efb905df58aa3a241df060c799cda1ff
kprashant <prashant98r@gmail.com>
2024-08-15 18:49:14 +0530

    To check the discount or membership eligibility

---
+++ src/main/scala/discountOrMembership.scala
@@ -0,0 +1,11 @@
+object discountOrMembership {
+  def main(args:Array[String]):Unit={
+    val amount = 180
+    if(amount>200){
+      println("You are eligible for discount")
+    }
+    else{
+      println("Loyality card available")
+    }
+  }
+}
